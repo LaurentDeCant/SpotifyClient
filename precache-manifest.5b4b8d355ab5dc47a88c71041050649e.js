f9e9a7d4dbca85d2a65fcfd07e15a462061e14a9
Laurent De Cant <laurent_de_cant@hotmail.com>
2019-03-17 11:47:48 -0400

    Updates

---
+++ precache-manifest.5b4b8d355ab5dc47a88c71041050649e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a854658ee7c499afaae5",
+    "url": "/spotify-client/static/js/main.a854658e.chunk.js"
+  },
+  {
+    "revision": "3f7854e2c63fa3a1dc30",
+    "url": "/spotify-client/static/js/runtime~main.3f7854e2.js"
+  },
+  {
+    "revision": "6b9fe74fc536be97aef9",
+    "url": "/spotify-client/static/js/2.6b9fe74f.chunk.js"
+  },
+  {
+    "revision": "7154c2073383438db77e8fb2de72fb20",
+    "url": "/spotify-client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d9df1640549fd2789896eb8fa1c23b0.js"
+  "/spotify-client/precache-manifest.5b4b8d355ab5dc47a88c71041050649e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/spotify-client/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
