f29a7f9efeffdebb0391f8dc7544a46d2e4e9a03
Laurent De Cant <laurent_de_cant@hotmail.com>
2019-05-14 13:46:45 -0400

    Updates

---
+++ precache-manifest.b006b29646ceba41a063819706fbf565.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d377427f1c1fa32dca96e16ed2c3111",
+    "url": "/spotify-client/index.html"
+  },
+  {
+    "revision": "56fbfb5b14e1049f60f0",
+    "url": "/spotify-client/static/js/2.ed841812.chunk.js"
+  },
+  {
+    "revision": "b326729d75b580b135cc",
+    "url": "/spotify-client/static/js/main.1aa68a9b.chunk.js"
+  },
+  {
+    "revision": "871e2d1f553c4b4ebe80",
+    "url": "/spotify-client/static/js/runtime~main.9f025a71.js"
+  }
+]);
