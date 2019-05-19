b8a426af985e11493004749ae30f423db4bed544
Laurent De Cant <laurent_de_cant@hotmail.com>
2019-05-19 18:28:42 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotify-client/precache-manifest.ec1aa9ab900064de8ea533b08911230b.js"
+  "/spotify-client/precache-manifest.d5c8a6cd6f999f8f81942a9c861b424c.js"
 );
 
 self.addEventListener('message', (event) => {
